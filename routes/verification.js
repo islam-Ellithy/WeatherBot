583d1d7027318e01c54615f64e58c32464fee7d4
Islam Emam <islam.emmo@gmail.com>
2018-05-27 13:36:08 +0200

    with webhook v3

--- routes/verification.js
+++ routes/verification.js
@@ -7,8 +7,22 @@ router.get("/", function (req, res, next) {
     if (req.query['hub.verify_token'] === 'try') {
         res.send(req.query['hub.challenge']);
     } else {
-        res.send("requested recevied");
+        res.send("requested recevied new");
     }
+});
+
+router.post("/", function (req, res, next) {
+
+    var msg_events = req.body.entry;    
+
+    msg_events.array.forEach(element => {
+        element.messaging.array.forEach(msg => {
+            console.log(msg);
+            res.sendStatus(200);
+            res.send(msg);
+        });
+        
+    });
 });
 
 module.exports = router;
