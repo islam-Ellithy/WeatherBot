342aefd28b7b3c11b216decacb25ecb56327aca8
Islam Emam <islam.emmo@gmail.com>
2018-05-30 18:14:40 +0200

    v75

--- routes/verification.js
+++ routes/verification.js
@@ -67,14 +67,14 @@ router.post("/", function (req, res, next) {
 
                             if (flag) {
                                 var restUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=c550788d001ff159854a8faa1a4066b7&mode=json&units=metric&q=' + city;
-                                getWeather(senderId, 'restUrl');
+                                getWeather(senderId, 'london');
                             }
 
                         } else if (attachment === 'location') {
-                            var cord = msg.attachments[0].payload.coordinates;
-                            var restUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=c550788d001ff159854a8faa1a4066b7&mode=json&units=metric&lat=' + '31.24' + '&lon=' + '30.05';
+                            //var cord = msg.attachments[0].payload.coordinates;
+                            //var restUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=c550788d001ff159854a8faa1a4066b7&mode=json&units=metric&lat=' + '31.24' + '&lon=' + '30.05';
 
-                            //getWeather(senderId, 'restUrl');
+                            getWeather(senderId, 'restUrl');
                         } else {
                             sendText(senderId, result);
                         }
